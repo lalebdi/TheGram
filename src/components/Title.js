c7ba60f73103392f4e6293b5fdb816cde08ebaa2
lalebdi <loai.alebdi@gmail.com>
2020-08-17 15:27:20 -0400

    styling

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>LeahGram</h1>
+      <h2>A simple & fun way to share photos</h2>
+      <p>Share your posts by clicking the button below</p>
     </div>
   )
 }

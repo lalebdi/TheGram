8c909fbde25e6fe496ffd7512bd9d428a09a1a07
lalebdi <loai.alebdi@gmail.com>
2020-08-16 21:10:47 -0400

    built a storage component

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react;
+import { projectStorage } from '../firebase/config';
+// our hooks are for handling the uploads
+
+const useStorage = (file) => {
+    const [progress, setProgress] = useState(0);
+    const [error, setError] = useState(null);
+    const [url, setUrl] = useState(null);
+
+    useEffect(() => {
+        // refernces
+        const storageRef = projectStorage.ref(file.name);
+        // this will upload the file to the refrence. it is async
+        storageRef.put(file).on('state_changed', (snap) => {
+            let percentage = (snap.bytesTransferred / snap.totalBytes)* 100;
+            setProgress(percentage);
+        }, (err) =>{
+            setError(err);
+        }, async() => {
+            const url = await storageRef.getDownloadURL(); //the image url will be saved in the firebase
+            setUrl(url);
+        })
+    }, [file])
+
+    return { progress, url, error }
+}
+
+export default useStorage;

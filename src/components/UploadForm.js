d98afa89e33af46a30d9395b7108bd7df2252fb5
lalebdi <loai.alebdi@gmail.com>
2020-08-16 20:45:33 -0400

    created an upload component

---
+++ src/components/UploadForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const UploadForm = () => {
+    const [file, setFile]= useState(null);
+    const [error , setError] = useState(null);
+    // below I'm selecting what's allowed to be uploaded
+    const types = ['image/png' , 'image/jpeg'];
+
+    const changeHandler = (e) => {
+        // console.log("changed");
+        let selected = e.target.files[0];
+        // console.log(selected);
+        // the if statement below is to ensure that the state is updated when a file is selected
+        if(selected && types.includes(selected.type)){
+            setFile(selected);
+            setError(''); // this is so the error message disappear one an image file is selected
+        } else{
+            setFile(null); // reseting the file
+            setError('Please select an image file');
+        }
+    }
+
+    return (
+        <form>
+            <input type="file" onChange={changeHandler} />
+            <div className="output">
+                { error && <div className="error"> {error} </div> }
+                { file && <div> {file.name} </div> }
+            </div>
+        </form>
+    )
+}
+
+export default UploadForm

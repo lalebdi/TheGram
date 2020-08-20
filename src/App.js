bf2134abbfa4cf38be6dc6d9095498edece00cd5
lalebdi <loai.alebdi@gmail.com>
2020-08-20 15:55:46 -0400

    added the profile component and fixed the auth bug

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import UploadForm from './components/UploadForm';
 import ImageGrid from './components/ImageGrid';
 import Modal from './components/Model';
 import LoginButton from './components/LoginButton'
+import LogoutButton from './components/LogoutButton'
+import Profile from './components/Profile';
 
 function App() {
   const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div className="App">
       <LoginButton />
+      <LogoutButton />
+      <Profile />
       <Title/>
       <UploadForm />
       <ImageGrid setSelectedImg={setSelectedImg} />
--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const LogoutButton = () => {
+    const { logout } = useAuth0();
     return (
-        <div>
-            
-        </div>
+        <button onClick={() => logout()}>
+            logout
+        </button>
     )
 }
 

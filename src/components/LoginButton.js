985a4db7f204d930ba42b91eb62353c2e4a4cd98
lalebdi <loai.alebdi@gmail.com>
2020-08-20 10:39:06 -0400

    initiated the auth provider

---
+++ src/components/LoginButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LoginButton = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LoginButton
--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+//  Auth0 uses React context will be able to access properties within the app.
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+  domain = {domain}
+  clientId = {clientId}
+  redirectUri={window.location.origin}>
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root')
 );
 

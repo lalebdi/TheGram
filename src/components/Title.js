47988ccd1e0c0099dcc6f745fd47262ca85dee29
lalebdi <loai.alebdi@gmail.com>
2020-08-20 20:24:05 -0400

    styled the background and the title

--- src/components/Title.js
+++ src/components/Title.js
@@ -6,7 +6,7 @@ const Title = () => {
   return (
     isAuthenticated && (
     <div className="title">
-      <h1>LeahGram</h1>
+      <h1><strong> LeahGram </strong></h1>
       <h2>A simple & fun way to share photos</h2>
       <p>Share your posts by clicking the button below</p>
     </div>

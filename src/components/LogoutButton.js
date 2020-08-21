a5d6029cd6a099c2ff398379998481683b4b7409
lalebdi <loai.alebdi@gmail.com>
2020-08-21 14:31:02 -0400

    styled the top bar

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -5,7 +5,7 @@ const LogoutButton = () => {
     const { logout, isAuthenticated } = useAuth0();
     return (
         isAuthenticated && (
-        <button onClick={() => logout()} className="Logout-btn" >
+        <button onClick={() => logout()} className="Logout-btn" style={{ width: "80px", height: "50px", borderRadius: "16px" }} >
             logout
         </button>
         )
